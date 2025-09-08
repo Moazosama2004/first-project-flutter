14973924277e96705df01d029c03052065ac273a
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-09-08 15:53:04 +0300

    column widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,16 @@ class FirstApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hello, World!'))),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(color: Colors.green),
+            child: Center(child: Text('Hello, World!')),
+          ),
+        ),
+      ),
     );
   }
 }
